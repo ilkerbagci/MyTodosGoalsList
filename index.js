26a3501a270fa20ef6e574b2c9f2d3ae90eb6afb
İlker Bağcı <ilkerbagci0@gmail.com>
2020-02-15 18:09:18 +0300

    Add 'Remove' and 'Toggle' actions to todo items

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
 // Library Code
-function createStore(reducer) {
+function createStore ( reducer) {
     // The store should have four parts
     // 1. The state
     // 2. Get the state.
     // 3. Listen to changes on the state.
     // 4. Update the state
-
+  
     let state
     let listeners = []
-
+  
     const getState = () => state
-
+  
     const subscribe = (listener) => {
-        listeners.push(listener)
-        return () => {
-            listeners = listeners.filter((l) => l !== listener)
-        }
+      listeners.push(listener)
+      return () => {
+        listeners = listeners.filter((l) => l !== listener)
+      }
     }
-
-    const dispatch = (action) => {
-        state = reducer(state, action)
-        listeners.forEach((listener) => listener())
+  
+    const  dispatch = (action) => {
+      state = reducer(state, action)
+      listeners.forEach((listener) => listener())
     }
-
+  
     return {
-        getState,
-        subscribe,
-        dispatch,
+      getState,
+      subscribe,
+      dispatch,
     }
-}
-
-
-//App code has to be a pure function
-//state will be undefined for first time usage. 
-//if undefined it should be an empty array.
-//also called as reducer function.
-function todos(state = [], action) {
-    if (action.type === 'ADD_TODO') {
+  }
+  
+  // App Code
+  function todos (state = [], action) {
+    switch(action.type) {
+      case 'ADD_TODO' :
         return state.concat([action.todo])
+      case 'REMOVE_TODO' :
+        return state.filter((todo) => todo.id !== action.id)
+      case 'TOGGLE_TODO' :
+        return state.map((todo) => todo.id !== action.id ? todo :
+          Object.assign({}, todo, { complete: !todo.complete }))
+      default :
+        return state
     }
-    return state
-}
-
-const store = createStore(todos)
-
-store.subscribe(() => {
+  }
+  
+  const store = createStore(todos)
+  
+  store.subscribe(() => {
     console.log('The new state is: ', store.getState())
-})
-
-store.dispatch({
+  })
+  
+  store.dispatch({
     type: 'ADD_TODO',
-    todo:{
-        id:0,
-        name:'Learn Redux',
-        complete:false
+    todo: {
+      id: 0,
+      name: 'Learn Redux',
+      complete: false
     }
-})
+  })
